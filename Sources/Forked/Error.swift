ae0bde752f8f2cdcf6f82d00c064159ec13c5f5c
Drew McCormack <drewmccormack@mac.com>
2024-08-02 11:06:17 +0200

    More comments.

--- Sources/Forked/Error.swift
+++ Sources/Forked/Error.swift
@@ -5,6 +5,7 @@ public enum Error: Swift.Error {
     case unexpectedError(any Swift.Error)
     case attemptToAccessNonExistentFork(Fork)
     case attemptToDeleteProtectedFork(Fork)
+    case attemptToDeleteAllDataFromMainFork
     case attemptToCreateExistingFork(Fork)
     case attemptToAccessNonExistentCommitInFork(Fork)
     case attemptToAccessNonExistentVersion(Version, Fork)
--- Sources/Forked/ForkedResource+Updating.swift
+++ Sources/Forked/ForkedResource+Updating.swift
@@ -3,6 +3,7 @@ import Foundation
 /// Thread-safe wrappers around repository methods
 public extension ForkedResource {
     
+    /// Create a new fork.Will throw if a fork with the same name already exists.
     func create(_ fork: Fork) throws {
         try serialize {
             guard !repository.forks.contains(fork) else {
@@ -12,6 +13,7 @@ public extension ForkedResource {
         }
     }
     
+    /// Delete an existing fork. If the fork does not exist, it will throw.
     func delete(_ fork: Fork) throws {
         try serialize {
             guard !fork.isProtected else { throw Error.attemptToDeleteProtectedFork(fork) }
@@ -19,18 +21,26 @@ public extension ForkedResource {
         }
     }
     
+    /// Update the contents of a fork with a new resource value. Will create a commit, and return the `Version`.
     @discardableResult func update(_ fork: Fork, with resource: ResourceType) throws -> Version {
         try serialize {
             try update(fork, with: .resource(resource))
         }
     }
     
+    /// Adds a new commit with content `.none`. This is like setting the content to `nil`.
+    /// Note that this does not remove the fork, and the fork does still have commits. However, the value of the
+    /// most recent commit will be `.none`, to indicate the absence of a resource.
+    /// (This construction is sometimes referred to as a "tombstone". It is a commit that indicates
+    /// that something has been removed.)
     @discardableResult func removeContent(from fork: Fork) throws -> Version {
         try serialize {
             try update(fork, with: .none)
         }
     }
     
+    /// Update the contents of a fork with a new resource value, or `.none` to indicate removal of a resource.
+    /// Will create a commit, and return the `Version`.
     @discardableResult func update(_ fork: Fork, with content: CommitContent<ResourceType>) throws -> Version {
         try serialize {
             func addNewCommit() throws {
@@ -61,10 +71,11 @@ public extension ForkedResource {
 }
 
 internal extension ForkedResource {
-    /// Removes any commits that no longer play a role
+    /// Removes any commits that no longer play a role.
     /// Main should only ever have one commit in it - no common ancestor.
     /// Other branches hold the common ancestors, so they will have zero (ie same as main),
-    /// or two or more commits (ie common ancestor, most recent, and — temporarily — inbetweens)
+    /// one (ie a comon anacestor),
+    /// or two (ie common ancestor and a recent commit).
     func removeRedundantCommits(in fork: Fork) throws {
         try serialize {
             let versions = try repository.ascendingVersions(storedIn: fork)
@@ -75,8 +86,8 @@ internal extension ForkedResource {
         }
     }
     
-    /// The current commit on main is copied to any empty forks in the repo, to form a common ancestor
-    /// This should be called anytime main is about to be updated
+    /// The current commit on main is copied to any empty forks in the repo, to form a common ancestor.
+    /// This should be called anytime main is about to be updated.
     func addCommonAncestorsToEmptyForks() throws {
         for fork in forks where fork != .main {
             let versions = try repository.versions(storedIn: fork)
@@ -90,7 +101,7 @@ internal extension ForkedResource {
     /// merged into main, and that they are at the same version.
     func removeAllCommits(in fork: Fork) throws {
         try serialize {
-            guard fork != .main else { throw Error.attemptToDeleteProtectedFork(fork) }
+            guard fork != .main else { throw Error.attemptToDeleteAllDataFromMainFork }
             let versions = try repository.ascendingVersions(storedIn: fork)
             try versions.forEach {
                 try repository.removeCommit(at: $0, from: fork)
@@ -98,6 +109,7 @@ internal extension ForkedResource {
         }
     }
     
+    /// Deletes all commits in a fork except the most recent one.
     func removeAllCommitsExceptMostRecent(in fork: Fork) throws {
         try serialize {
             let versions = try repository.ascendingVersions(storedIn: fork)
@@ -108,6 +120,8 @@ internal extension ForkedResource {
         }
     }
     
+    /// Removes the common ancestor in a fork. It has no effect on the main fork,
+    /// because that does not store a common ancestor.
     func removeCommonAncestor(in fork: Fork) throws {
         try serialize {
             guard fork != .main else { return }
