23797e51ee759534c744e0ab091514eb8dc3b270
Drew McCormack <drewmccormack@mac.com>
2024-08-02 11:25:51 +0200

    More documentation.

--- Sources/Forked/MergeAction.swift
+++ Sources/Forked/MergeAction.swift
@@ -1,8 +1,18 @@
 import Foundation
 
+/// The action taken when two forks are merged.
 public enum MergeAction: Equatable, Sendable {
+    /// No action was taken. The two forks were already at the same version.
     case none
+    
+    /// One of the forks was ahead of the other, and the other had no new commits.
+    /// So the fork with older version was simply made equal to the newer fork version.
+    /// This is known as a "fast forward".
     case fastForward
+    
+    /// The two forks had both changed since the common ancestor version.
+    /// A `Resolver` was used to merge the two, with the new commit added
+    /// to the appropriate fork.
     case resolveConflict
 }
 
--- Sources/Forked/Resolver.swift
+++ Sources/Forked/Resolver.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 /// Conforming types are capable of resolving conflicts during a merge.
+/// You can use an existing `Resolver` or make your own to do a
+/// custom merge of your resource.
 public protocol Resolver {
+    
+    /// Merges the conflicting commits and returns a new commit for the result.
+    /// The common ancestor holds the resource value when the two forks last
+    /// contained the same version.
     func mergedContent<R: Resource>(forConflicting commits: (Commit<R>, Commit<R>), withCommonAncestor ancestorCommit: Commit<R>) throws -> CommitContent<R>
+    
 }
