dd3d5671e0469df0409fa1d774633929ae1abd25
Drew McCormack <drewmccormack@mac.com>
2024-08-30 12:14:35 +0200

    Implemented equality and comparison in Version.
    It now only considers the count, not the time stamp, since the count should always be unique for any give version.

--- Sources/Forked/Version.swift
+++ Sources/Forked/Version.swift
@@ -7,15 +7,22 @@ public struct Version: Comparable, Hashable, Sendable, Codable {
     
     /// Big bang version of every ForkedResource. Effectively, it is ancient history.
     /// Also used as the initial value of any newly created branch
-    public static let initialVersion: Version =
-        .init(count: 0, timestamp: .distantPast)
+    public static let initialVersion: Version = .init(count: 0, timestamp: .distantPast)
     
     /// Increase the timestamp by 1
     public func next() -> Version {
         .init(count: count+1, timestamp: .now)
     }
     
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(count)
+    }
+    
+    public static func == (lhs: Version, rhs: Version) -> Bool {
+        lhs.count == rhs.count
+    }
+    
     public static func < (lhs: Version, rhs: Version) -> Bool {
-        (lhs.count, lhs.timestamp) < (rhs.count, rhs.timestamp)
+        lhs.count < rhs.count
     }
 }
