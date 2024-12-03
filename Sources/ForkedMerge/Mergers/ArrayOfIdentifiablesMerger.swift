582faa775dde1042a18b7d7503860d1213dfd935
Drew McCormack <drewmccormack@mac.com>
2024-12-03 15:07:57 +0100

    Added ArrayOfIdentifiablesMerger.
    This can be used to merge arrays containing identifiables.

---
+++ Sources/ForkedMerge/Mergers/ArrayOfIdentifiablesMerger.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+/// Merges an array, ensuring that the result has elements with unqiue identifiers.
+public struct ArrayOfIdentifiablesMerger<Element: Identifiable & Equatable>: Merger {
+    
+    public init() {}
+
+    public func merge(_ value: [Element], withOlderConflicting other: [Element], commonAncestor: [Element]?) throws -> [Element] {
+        guard let commonAncestor else { return value }
+        var v1: MergeableArray<Element> = .init(commonAncestor)
+        var v2 = v1
+        v2.values = other
+        v1.values = value
+        return v1.merged(with: v2).entriesUniquelyIdentified().values
+    }
+    
+}
+
---
+++ Tests/ForkedTests/ArrayOfIdentifiablesMerger.swift
@@ -0,0 +1,61 @@
+import Testing
+import Foundation
+import Forked
+@testable import ForkedMerge
+
+struct ArrayOfIdentifiableMergerSuite {
+    let ancestor: [Item] = [1, 2, 3].itemsArray
+    let merger = ArrayOfIdentifiablesMerger<Item>()
+    
+    @Test func mergeOneSidedAppend() throws {
+        let updated = [1, 2, 3, 3, 4].itemsArray
+        let merged = try merger.merge(updated, withOlderConflicting: ancestor, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1, 2, 3, 4])
+    }
+    
+    @Test func mergeOneSidedRemove() throws {
+        let updated = [1, 3].itemsArray
+        let merged = try merger.merge(updated, withOlderConflicting: ancestor, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1, 3])
+    }
+    
+    @Test func mergeOneSidedAddAndRemove() throws {
+        let updated = [1, 3, 4].itemsArray
+        let merged = try merger.merge(updated, withOlderConflicting: ancestor, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1, 3, 4])
+    }
+    
+    @Test func mergeTwoSidedInsert() throws {
+        let updated1 = [1, 2, 4, 3].itemsArray
+        let updated2 = [1, 2, 4, 3, 5].itemsArray
+        let merged = try merger.merge(updated2, withOlderConflicting: updated1, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1, 2, 4, 3, 5])
+    }
+    
+    @Test func mergeTwoSidedDeletes() throws {
+        let updated1 = [1, 2, 1].itemsArray
+        let updated2 = [1, 3, 1].itemsArray
+        let merged = try merger.merge(updated2, withOlderConflicting: updated1, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1])
+    }
+    
+    @Test func mergeTwoSidedInsertAndDelete() throws {
+        let updated1 = [1, 2, 4].itemsArray
+        let updated2 = [1, 5, 3].itemsArray
+        let merged = try merger.merge(updated2, withOlderConflicting: updated1, commonAncestor: ancestor)
+        #expect(merged.map({ $0.value }) == [1, 5, 4])
+    }
+}
+
+struct Item: Identifiable, Equatable {
+    let id: String
+    let value: Int
+}
+
+extension Array where Element == Int {
+    
+    var itemsArray: [Item] {
+        map { .init(id: "\($0)", value: $0) }
+    }
+    
+}
