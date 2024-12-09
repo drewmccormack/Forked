4e7a0c895425bcd72c23e2bb816844b30b0ca10d
Drew McCormack <drewmccormack@mac.com>
2024-12-09 20:49:00 +0100

    Changes suggested by cursor for docs to work.

--- bin/build-docs.zsh
+++ bin/build-docs.zsh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
-swift package --allow-writing-to-directory ./docs \
-generate-documentation \
+swift package \
+    --allow-writing-to-directory ./docs \
+    generate-documentation \
     --target Forked --target ForkedMerge --target ForkedModel --target ForkedCloudKit \
+    --output-path ./docs \
+    --emit-digest \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path Forked \
-    --output-path ./docs \
+    --enable-inherited-docs
+
+# Create .nojekyll file to prevent GitHub Pages from ignoring files that begin with an underscore
+touch ./docs/.nojekyll
