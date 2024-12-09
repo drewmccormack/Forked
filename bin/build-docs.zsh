becf41afbff5e0af736298178da1892baff57ab8
Drew McCormack <drewmccormack@mac.com>
2024-12-09 21:06:14 +0100

    Attempting to get proper static HTML for all packages.

--- bin/build-docs.zsh
+++ bin/build-docs.zsh
@@ -3,19 +3,47 @@
 # First, clean the docs directory to ensure fresh generation
 rm -rf ./docs/*
 
-swift package \
-    --allow-writing-to-directory ./docs \
-    generate-documentation \
-    --target Forked --target ForkedMerge --target ForkedModel --target ForkedCloudKit \
-    --output-path ./docs \
-    --emit-digest \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path Forked \
-    --enable-inherited-docs
+# Array of targets
+targets=("Forked" "ForkedMerge" "ForkedModel" "ForkedCloudKit")
+
+# Generate documentation for each target separately
+for target in $targets; do
+    swift package \
+        --allow-writing-to-directory ./docs \
+        generate-documentation \
+        --target $target \
+        --output-path "./docs/$target" \
+        --emit-digest \
+        --disable-indexing \
+        --transform-for-static-hosting \
+        --hosting-base-path "$target" \
+        --enable-inherited-docs
+done
 
 # Create .nojekyll file to prevent GitHub Pages from ignoring files that begin with an underscore
 touch ./docs/.nojekyll
+
+# Create a simple index.html that links to each package's documentation
+cat > ./docs/index.html << EOL
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Documentation</title>
+</head>
+<body>
+    <h1>Available Documentation</h1>
+    <ul>
+EOL
+
+for target in $targets; do
+    echo "        <li><a href='./$target/documentation/$target/'>${target}</a></li>" >> ./docs/index.html
+done
+
+cat >> ./docs/index.html << EOL
+    </ul>
+</body>
+</html>
+EOL
 
 # Verify the output structure
 ls -la ./docs
